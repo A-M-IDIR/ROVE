bda16bd128bb3640f643a1ec652b400dc97184cf
A-M-IDIR <am.idir.contact@gmail.com>
2024-11-08 16:08:01 +0100

    feat: set-up main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./style.scss";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  return <div className="App"></div>;
+}
+
+createRoot(document.getElementById("root")!).render(<App />);

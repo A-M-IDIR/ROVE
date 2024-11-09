b3ba2631d3763bb91b4a88f9f9e0dbde8a8a2d63
A-M-IDIR <am.idir.contact@gmail.com>
2024-11-09 15:00:50 +0100

    feat: set-up main

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,39 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { AnimatePresence } from "framer-motion";
+
+import WtihAlert from "RESOURCES/HOCS/WtihAlert";
+
 import "./style.scss";
 
 function App() {
-  return <div className="App"></div>;
+  React.useEffect(() => {
+    document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+    return () => {
+      document.removeEventListener("contextmenu", (event) =>
+        event.preventDefault()
+      );
+    };
+  }, []);
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<></>} />
+      </Routes>
+    </AnimatePresence>
+  );
 }
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={new QueryClient()}>
+    <BrowserRouter>
+      <WtihAlert>
+        <App />
+      </WtihAlert>
+    </BrowserRouter>
+  </QueryClientProvider>
+);

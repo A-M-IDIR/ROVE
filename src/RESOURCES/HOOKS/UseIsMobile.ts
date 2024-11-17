fb41407fb4c0aaa15fed11c2a86d0a2345f2a126
A-M-IDIR <am.idir.contact@gmail.com>
2024-11-17 21:15:08 +0100

    feat: clean-up and add description

--- src/RESOURCES/HOOKS/UseIsMobile.ts
+++ src/RESOURCES/HOOKS/UseIsMobile.ts
@@ -1,9 +1,16 @@
-import { useState, useEffect } from "react";
+import React from "react";
 
+/**
+ * Custom React Hook: `UseIsMobile`
+ *
+ * This hook determines whether the current device is a mobile device based on the user agent string.
+ *
+ * @returns {boolean} `true` if the user is on a mobile device, otherwise `false`.
+ */
 const UseIsMobile = (): boolean => {
-  const [isMobile, setIsMobile] = useState<boolean>(false);
+  const [isMobile, setIsMobile] = React.useState<boolean>(false);
 
-  useEffect(() => {
+  React.useEffect(() => {
     const userAgent =
       navigator.userAgent || navigator.vendor || (window as any).opera;
 
